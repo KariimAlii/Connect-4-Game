95355243abfef059c373f1983dbdbc0dcc9b8ef2
Karim Ali <kariimalii96@gmail.com>
2023-02-26 22:59:39 +0200

    Updated Server Logic

--- Connect4Game/Server.Designer.cs
+++ Connect4Game/Server.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.StatusBox = new System.Windows.Forms.TextBox();
+            this.ConnectionLabel = new System.Windows.Forms.Label();
+            this.StopBtn = new System.Windows.Forms.Button();
+            this.ListenBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // StatusBox
+            // 
+            this.StatusBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StatusBox.ForeColor = System.Drawing.SystemColors.Window;
+            this.StatusBox.Location = new System.Drawing.Point(307, 282);
+            this.StatusBox.Name = "StatusBox";
+            this.StatusBox.Size = new System.Drawing.Size(200, 24);
+            this.StatusBox.TabIndex = 10;
+            // 
+            // ConnectionLabel
+            // 
+            this.ConnectionLabel.AutoSize = true;
+            this.ConnectionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConnectionLabel.ForeColor = System.Drawing.Color.Red;
+            this.ConnectionLabel.Location = new System.Drawing.Point(232, 284);
+            this.ConnectionLabel.Name = "ConnectionLabel";
+            this.ConnectionLabel.Size = new System.Drawing.Size(69, 20);
+            this.ConnectionLabel.TabIndex = 9;
+            this.ConnectionLabel.Text = "Status:";
+            // 
+            // StopBtn
+            // 
+            this.StopBtn.BackColor = System.Drawing.Color.IndianRed;
+            this.StopBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StopBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.StopBtn.Location = new System.Drawing.Point(655, 79);
+            this.StopBtn.Name = "StopBtn";
+            this.StopBtn.Size = new System.Drawing.Size(113, 41);
+            this.StopBtn.TabIndex = 12;
+            this.StopBtn.Text = "Stop";
+            this.StopBtn.UseVisualStyleBackColor = false;
+            this.StopBtn.Click += new System.EventHandler(this.StopBtn_Click);
+            // 
+            // ListenBtn
+            // 
+            this.ListenBtn.BackColor = System.Drawing.Color.Chartreuse;
+            this.ListenBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ListenBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ListenBtn.Location = new System.Drawing.Point(655, 36);
+            this.ListenBtn.Name = "ListenBtn";
+            this.ListenBtn.Size = new System.Drawing.Size(113, 37);
+            this.ListenBtn.TabIndex = 11;
+            this.ListenBtn.Text = "Start";
+            this.ListenBtn.UseVisualStyleBackColor = false;
+            this.ListenBtn.Click += new System.EventHandler(this.ListenBtn_Click);
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.StopBtn);
+            this.Controls.Add(this.ListenBtn);
+            this.Controls.Add(this.StatusBox);
+            this.Controls.Add(this.ConnectionLabel);
+            this.Name = "Server";
             this.Text = "Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.TextBox StatusBox;
+        private System.Windows.Forms.Label ConnectionLabel;
+        private System.Windows.Forms.Button StopBtn;
+        private System.Windows.Forms.Button ListenBtn;
     }
 }
--- Connect4Game/Server.cs
+++ Connect4Game/Server.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,12 +17,59 @@ namespace Connect4Game
     public partial class Server : Form
     {
         TcpListener listener;
+        NetworkStream stream;
+        StreamWriter writer;
+        StreamReader reader;
+        SynchronizationContext context;
         public Server()
         {
             InitializeComponent();
 
             IPAddress ip = new IPAddress(new byte[] { 192, 168, 1, 5 });
             listener = new TcpListener(ip, 5000);
+
+            context = SynchronizationContext.Current;
+        }
+
+
+        private void AcceptConnections()
+        {
+            while (true)
+            {
+                TcpClient serverClient = listener.AcceptTcpClient();
+                stream = serverClient.GetStream();
+
+                writer = new StreamWriter(stream);
+                writer.AutoFlush = true;
+                writer.Write("Connected");
+
+                reader = new StreamReader(stream);
+
+                context.Post((object obj) => StatusBox.BackColor = Color.Chartreuse, null);
+                context.Post((object obj) => StatusBox.Text = "Connection Accepted!", null);
+            }
+        }
+
+        private void ListenBtn_Click(object sender, EventArgs e)
+        {
+            listener.Start();
+
+            Task.Run(() => AcceptConnections());
+
+            context.Post((object obj) => StatusBox.BackColor = Color.Chartreuse, null);
+            context.Post((object obj) => StatusBox.Text = "Listening...", null);
+        }
+
+        private void StopBtn_Click(object sender, EventArgs e)
+        {
+            StopListening();
+        }
+        private void StopListening()
+        {
+            listener.Stop();
+            writer.Write("ServerStopped");
+            context.Post((object obj) => StatusBox.BackColor = Color.IndianRed, null);
+            context.Post((object obj) => StatusBox.Text = "Server Stopped...", null);
         }
     }
 }
