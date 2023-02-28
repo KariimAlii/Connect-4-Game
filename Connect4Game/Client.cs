edbedd497090b89166e129c8f07657043b31c35f
Karim Ali <kariimalii96@gmail.com>
2023-02-28 18:25:48 +0200

    Updated Client/Server Connection Logic

--- Connect4Game/Client.cs
+++ Connect4Game/Client.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,46 +14,43 @@ namespace Connect4Game
 {
     internal class Client
     {
-         public TcpClient tcpClient;
+        public TcpClient tcpClient;
         public string name { get; set; }
         public string number { get; set; }
+
         NetworkStream stream;
         StreamWriter writer;
         StreamReader reader;
+        Thread streamingThread;
+
         public BackgroundWorker backgroundWorker2;
 
-        public Client(TcpClient tcpClient,string name,string number)
+        public Client(TcpClient tcpClient)
         {
             this.tcpClient = tcpClient;
-            this.name = name;
-            this.number = number;
-             stream = tcpClient.GetStream();
-             writer = new StreamWriter(stream);
+
+            stream = tcpClient.GetStream();
+
+            writer = new StreamWriter(stream);
             writer.AutoFlush = true;
             writer.Write("Connected");
-             reader = new StreamReader(stream);
-            backgroundWorker2 = new BackgroundWorker();
-            backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
-            Task.Run(() => ReceiveMessages());
-            backgroundWorker2.WorkerSupportsCancellation= true;
-            
-        }
-        ~Client() {
-            backgroundWorker2.CancelAsync();
-            tcpClient.Close();
-        }
 
-        
-        private async void ReceiveMessages()
-        {
-            if (backgroundWorker2.IsBusy == false)
-            {
-                backgroundWorker2.RunWorkerAsync();
-            }
-        }
-        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
-        {
 
+            reader = new StreamReader(stream);
+
+            //Task.Run(() => ReceiveMessages());
+            streamingThread = new Thread(() => ReceiveMessages());
+            streamingThread.IsBackground = true;
+            streamingThread.Start();
+        }
+        //~Client() {
+        //    backgroundWorker2.CancelAsync();
+        //    tcpClient.Close();
+        //}
+
+
+        private void ReceiveMessages()
+        {
             while (true)
             {
                 if (stream != null)
@@ -60,13 +58,16 @@ namespace Connect4Game
                     char[] charArr = new char[100];
                     int x = reader.Read(charArr, 0, 100);
                     string str = new string(charArr);
+
+                    //string str = await reader.ReadLineAsync();
                     if (str.Contains("ClientStopped"))
                     {
                         MessageBox.Show("Your Client Stopped Playing!!");
-                        this.tcpClient.Close();
-                        //MessageBox.Show(this.tcpClient.Connected.ToString());
+                        this.tcpClient.Dispose();
+                        streamingThread.Abort();
+                        //this.tcpClient.Close();
 
-                        
+                        //MessageBox.Show(this.tcpClient.Connected.ToString());
                     }
 
                     else if (str.Contains(","))
@@ -82,5 +83,6 @@ namespace Connect4Game
 
             }
         }
+
     }
 }
--- Connect4Game/Server.Designer.cs
+++ Connect4Game/Server.Designer.cs
@@ -33,18 +33,18 @@
             this.StopBtn = new System.Windows.Forms.Button();
             this.ListenBtn = new System.Windows.Forms.Button();
             this.clients_list = new System.Windows.Forms.ListBox();
-            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.connected_clients = new System.Windows.Forms.Button();
+            this.OpenGame = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // StatusBox
             // 
             this.StatusBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.StatusBox.ForeColor = System.Drawing.SystemColors.Window;
-            this.StatusBox.Location = new System.Drawing.Point(230, 229);
-            this.StatusBox.Margin = new System.Windows.Forms.Padding(2);
+            this.StatusBox.Location = new System.Drawing.Point(307, 282);
+            this.StatusBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.StatusBox.Name = "StatusBox";
-            this.StatusBox.Size = new System.Drawing.Size(151, 21);
+            this.StatusBox.Size = new System.Drawing.Size(200, 24);
             this.StatusBox.TabIndex = 10;
             // 
             // ConnectionLabel
@@ -52,10 +52,9 @@
             this.ConnectionLabel.AutoSize = true;
             this.ConnectionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ConnectionLabel.ForeColor = System.Drawing.Color.Red;
-            this.ConnectionLabel.Location = new System.Drawing.Point(174, 231);
-            this.ConnectionLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.ConnectionLabel.Location = new System.Drawing.Point(232, 284);
             this.ConnectionLabel.Name = "ConnectionLabel";
-            this.ConnectionLabel.Size = new System.Drawing.Size(59, 17);
+            this.ConnectionLabel.Size = new System.Drawing.Size(69, 20);
             this.ConnectionLabel.TabIndex = 9;
             this.ConnectionLabel.Text = "Status:";
             // 
@@ -64,10 +63,10 @@
             this.StopBtn.BackColor = System.Drawing.Color.IndianRed;
             this.StopBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.StopBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.StopBtn.Location = new System.Drawing.Point(491, 64);
-            this.StopBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.StopBtn.Location = new System.Drawing.Point(655, 79);
+            this.StopBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.StopBtn.Name = "StopBtn";
-            this.StopBtn.Size = new System.Drawing.Size(85, 33);
+            this.StopBtn.Size = new System.Drawing.Size(113, 41);
             this.StopBtn.TabIndex = 12;
             this.StopBtn.Text = "Stop";
             this.StopBtn.UseVisualStyleBackColor = false;
@@ -78,10 +77,10 @@
             this.ListenBtn.BackColor = System.Drawing.Color.Chartreuse;
             this.ListenBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ListenBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ListenBtn.Location = new System.Drawing.Point(491, 29);
-            this.ListenBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.ListenBtn.Location = new System.Drawing.Point(655, 36);
+            this.ListenBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.ListenBtn.Name = "ListenBtn";
-            this.ListenBtn.Size = new System.Drawing.Size(85, 30);
+            this.ListenBtn.Size = new System.Drawing.Size(113, 37);
             this.ListenBtn.TabIndex = 11;
             this.ListenBtn.Text = "Start";
             this.ListenBtn.UseVisualStyleBackColor = false;
@@ -90,35 +89,47 @@
             // clients_list
             // 
             this.clients_list.FormattingEnabled = true;
-            this.clients_list.Location = new System.Drawing.Point(95, 43);
+            this.clients_list.ItemHeight = 16;
+            this.clients_list.Location = new System.Drawing.Point(127, 53);
+            this.clients_list.Margin = new System.Windows.Forms.Padding(4);
             this.clients_list.Name = "clients_list";
-            this.clients_list.Size = new System.Drawing.Size(122, 95);
+            this.clients_list.Size = new System.Drawing.Size(161, 116);
             this.clients_list.TabIndex = 13;
-            // 
-            // backgroundWorker1
-            // 
             // 
             // connected_clients
             // 
-            this.connected_clients.Location = new System.Drawing.Point(249, 204);
+            this.connected_clients.Location = new System.Drawing.Point(332, 251);
+            this.connected_clients.Margin = new System.Windows.Forms.Padding(4);
             this.connected_clients.Name = "connected_clients";
-            this.connected_clients.Size = new System.Drawing.Size(109, 20);
+            this.connected_clients.Size = new System.Drawing.Size(145, 25);
             this.connected_clients.TabIndex = 14;
             this.connected_clients.Text = "connected_clients";
             this.connected_clients.UseVisualStyleBackColor = true;
+            this.connected_clients.Click += new System.EventHandler(this.connected_clients_Click);
+            // 
+            // OpenGame
+            // 
+            this.OpenGame.Location = new System.Drawing.Point(618, 300);
+            this.OpenGame.Name = "OpenGame";
+            this.OpenGame.Size = new System.Drawing.Size(75, 23);
+            this.OpenGame.TabIndex = 15;
+            this.OpenGame.Text = "Game";
+            this.OpenGame.UseVisualStyleBackColor = true;
+            this.OpenGame.Click += new System.EventHandler(this.OpenGame_Click);
             // 
             // Server
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.OpenGame);
             this.Controls.Add(this.connected_clients);
             this.Controls.Add(this.clients_list);
             this.Controls.Add(this.StopBtn);
             this.Controls.Add(this.ListenBtn);
             this.Controls.Add(this.StatusBox);
             this.Controls.Add(this.ConnectionLabel);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Server";
             this.Text = "Server";
             this.ResumeLayout(false);
@@ -132,7 +143,7 @@
         private System.Windows.Forms.Button StopBtn;
         private System.Windows.Forms.Button ListenBtn;
         private System.Windows.Forms.ListBox clients_list;
-        private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.Button connected_clients;
+        private System.Windows.Forms.Button OpenGame;
     }
 }
